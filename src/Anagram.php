fec2df55e54a08dc9f30e795fa4cb7f7f54d62fc
Ben Spenard & Mitch Rivet <student@epicodus.com>
2015-08-11 11:55:08 -0700

    add spec and test for single letter input (passed)

--- src/Anagram.php
+++ src/Anagram.php
@@ -1,21 +1,33 @@
 <?php
 
-    class Anagram {
 
+    class Anagram
+    {
         function findAnagram($input_word)
-
+      {
         include "anagram_library.php";
 
         $array_of_letters = str_split($input_word);
-        $output = array();
+        $output = "";
 
+        foreach ($library as $single_word)
+        {
 
-
-        foreach ($array_of_letters as $letter) {
-          
-
-          if () {
+          $split_word = str_split($single_word);
+          if(in_array($array_of_letters, $split_word))
+          {
+            $output_word = $single_word;
+            $output .= $output_word;
+          } else { $error_message = "No anagrams found.";
+                    $output .= $error_message;
 
           }
+
         }
+
+        return $output;
+      }
+
     }
+
+?>
