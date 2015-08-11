3f04b320a55fde4ccf0773f8a73d5e9e86259e4d
Ben Spenard & Mitch Rivet <student@epicodus.com>
2015-08-11 11:27:51 -0700

    findAnagram method draft

--- src/Anagram.php
+++ src/Anagram.php
@@ -2,5 +2,20 @@
 
     class Anagram {
 
-      function findAnagram()
+        function findAnagram($input_word)
+
+        include "anagram_library.php";
+
+        $array_of_letters = str_split($input_word);
+        $output = array();
+
+
+
+        foreach ($array_of_letters as $letter) {
+          
+
+          if () {
+
+          }
+        }
     }
---
+++ src/anagram_library.php
@@ -0,0 +1,7 @@
+<?php
+
+  $library = array("
+
+  ");
+
+?>
